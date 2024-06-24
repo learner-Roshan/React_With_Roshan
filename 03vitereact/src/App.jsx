b5ae63f23b6f02cd2575e398394bcf4f8586f9c8
Roshan Koirala <koiralaroshan321@gmail.com>
2024-06-24 16:38:47 +0545

    Learning Props with Tailwind CSS

--- 03vitereact/src/App.jsx
+++ 03vitereact/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-3 rounded-xl'>Roshan Codes in an interesting way!</h1>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-2'>Practicing Tailwind Css</h1>
+
+    <Card name="Roshan Koirala" des="I am currently studying in Pulchowk Engineering Campus"/>
+    <Card name="Koirala Roshan" des="I am currently studying in Admark Academy"/>
     </>
   )
 }
